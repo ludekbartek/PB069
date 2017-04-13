3f80b14f165779a3b41881f4cbb47339a741fb97
Ludek Bartek <bar@fi.muni.cz>
2017-04-13 10:35:03 +0200

    Pridana ukazka navrhu GUI pomoci WPF

---
+++ WPFLayoutsDemo/WPFLayoutsDemo/AddDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFLayoutsDemo
+{
+    /// <summary>
+    /// Interaction logic for AddDialog.xaml
+    /// </summary>
+    public partial class AddDialog : Window
+    {
+        public AddDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Dialog_OK_Click(object sender, RoutedEventArgs e)
+        {
+            if(input.Text!="" && input2.Text!="")
+            {
+                DialogResult = true;
+                return;
+            }
+            DialogResult = false;
+        }
+    }
+}
---
+++ WPFLayoutsDemo/WPFLayoutsDemo/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLayoutsDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            AddDialog dialog = new AddDialog();
+            dialog.ShowDialog();
+            if(dialog.DialogResult==true)
+            {
+                TextInput.Text = dialog.input.Text;
+                CountValue.Content = "1";
+            }
+            else
+            {
+                CountValue.Content = "0";
+            }
+        }
+    }
+}
