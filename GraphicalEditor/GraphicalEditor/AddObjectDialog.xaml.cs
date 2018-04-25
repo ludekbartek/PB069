e34f8ca4f62df9beb0389182e0c07e2dbe5414b5
Ludek Bartek <bar@fi.muni.cz>
2018-04-25 22:36:34 +0200

    Demo na 26.4. 2018

---
+++ GraphicalEditor/GraphicalEditor/AddObjectDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraphicalEditor
+{
+    /// <summary>
+    /// Interaction logic for AddObjectDialog.xaml
+    /// </summary>
+    public partial class AddObjectDialog : Window
+    {
+        public GraphicalObject GraphicalObject { get; set; }
+
+        public AddObjectDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckInputs())
+            {
+                GraphicalObject.X = int.Parse(XBox.Text);
+                GraphicalObject.Y = int.Parse(YBox.Text);
+                GraphicalObject.dx = int.Parse(DXBox.Text);
+                GraphicalObject.dy = int.Parse(DYBox.Text);
+                DialogResult = true;
+            }
+        }
+
+        private bool CheckInputs()
+        {
+            return XBox.Text != "" && YBox.Text != "" && DXBox.Text != "" && DYBox.Text != "";
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
