70fc1109564381eefc7dd999b18d57774ad76443
bar@fi.muni.cz <bar@fi.muni.cz>
2022-03-08 19:08:58 +0100

    Nastrel zadani pro 4. tyden

---
+++ 2022/4.tyden/Draw/Draw/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.souborToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.otevritToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ulozitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportovatDoJPEGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.caraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.obdelnikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elipsaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.souborToolStripMenuItem,
+            this.tvarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // souborToolStripMenuItem
+            // 
+            this.souborToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novyToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.otevritToolStripMenuItem,
+            this.ulozitToolStripMenuItem,
+            this.exportovatDoJPEGToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.souborToolStripMenuItem.Name = "souborToolStripMenuItem";
+            this.souborToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.souborToolStripMenuItem.Text = "Soubor";
+            // 
+            // novyToolStripMenuItem
+            // 
+            this.novyToolStripMenuItem.Name = "novyToolStripMenuItem";
+            this.novyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.novyToolStripMenuItem.Text = "Novy";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // otevritToolStripMenuItem
+            // 
+            this.otevritToolStripMenuItem.Name = "otevritToolStripMenuItem";
+            this.otevritToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.otevritToolStripMenuItem.Text = "Otevrit";
+            // 
+            // ulozitToolStripMenuItem
+            // 
+            this.ulozitToolStripMenuItem.Name = "ulozitToolStripMenuItem";
+            this.ulozitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ulozitToolStripMenuItem.Text = "Ulozit";
+            // 
+            // exportovatDoJPEGToolStripMenuItem
+            // 
+            this.exportovatDoJPEGToolStripMenuItem.Name = "exportovatDoJPEGToolStripMenuItem";
+            this.exportovatDoJPEGToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportovatDoJPEGToolStripMenuItem.Text = "Exportovat do JPEG";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // tvarToolStripMenuItem
+            // 
+            this.tvarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.caraToolStripMenuItem,
+            this.obdelnikToolStripMenuItem,
+            this.elipsaToolStripMenuItem});
+            this.tvarToolStripMenuItem.Name = "tvarToolStripMenuItem";
+            this.tvarToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.tvarToolStripMenuItem.Text = "Tvar";
+            // 
+            // caraToolStripMenuItem
+            // 
+            this.caraToolStripMenuItem.Name = "caraToolStripMenuItem";
+            this.caraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.caraToolStripMenuItem.Text = "Cara";
+            // 
+            // obdelnikToolStripMenuItem
+            // 
+            this.obdelnikToolStripMenuItem.Name = "obdelnikToolStripMenuItem";
+            this.obdelnikToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.obdelnikToolStripMenuItem.Text = "Obdelnik";
+            // 
+            // elipsaToolStripMenuItem
+            // 
+            this.elipsaToolStripMenuItem.Name = "elipsaToolStripMenuItem";
+            this.elipsaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.elipsaToolStripMenuItem.Text = "Elipsa";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 422);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem souborToolStripMenuItem;
+        private ToolStripMenuItem novyToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem otevritToolStripMenuItem;
+        private ToolStripMenuItem ulozitToolStripMenuItem;
+        private ToolStripMenuItem exportovatDoJPEGToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem tvarToolStripMenuItem;
+        private ToolStripMenuItem caraToolStripMenuItem;
+        private ToolStripMenuItem obdelnikToolStripMenuItem;
+        private ToolStripMenuItem elipsaToolStripMenuItem;
+        private PictureBox pictureBox1;
+    }
+}
