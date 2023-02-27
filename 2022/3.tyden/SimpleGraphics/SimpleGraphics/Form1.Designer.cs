59fcda78802b805bf9415d85f19ce650572828e4
bar@fi.muni.cz <bar@fi.muni.cz>
2023-02-27 19:52:37 +0100

    Priprava na 28.2. 2023

--- 2022/3.tyden/SimpleGraphics/SimpleGraphics/Form1.Designer.cs
+++ 2022/3.tyden/SimpleGraphics/SimpleGraphics/Form1.Designer.cs
@@ -1,7 +1,5 @@
-﻿namespace SimpleGraphics
-{
-    partial class Form1
-    {
+﻿namespace SimpleGraphics {
+    partial class Form1 {
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -11,10 +9,8 @@
         ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -26,155 +22,147 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
-            this.shapeComboBox = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.xStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.yStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dxTextBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dyTextBox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.statusStrip1.SuspendLayout();
-            this.SuspendLayout();
+        private void InitializeComponent() {
+            shapeComboBox = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            xStripStatusLabel = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            yStripStatusLabel = new ToolStripStatusLabel();
+            label1 = new Label();
+            dxTextBox = new TextBox();
+            label2 = new Label();
+            dyTextBox = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // shapeComboBox
             // 
-            this.shapeComboBox.FormattingEnabled = true;
-            this.shapeComboBox.Items.AddRange(new object[] {
-            "Cara",
-            "Obdelnik",
-            "Elipsa"});
-            this.shapeComboBox.Location = new System.Drawing.Point(17, 7);
-            this.shapeComboBox.Name = "shapeComboBox";
-            this.shapeComboBox.Size = new System.Drawing.Size(121, 23);
-            this.shapeComboBox.TabIndex = 0;
+            shapeComboBox.FormattingEnabled = true;
+            shapeComboBox.Items.AddRange(new object[] { "Cara", "Obdelnik", "Elipsa" });
+            shapeComboBox.Location = new Point(17, 7);
+            shapeComboBox.Name = "shapeComboBox";
+            shapeComboBox.Size = new Size(121, 23);
+            shapeComboBox.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(153, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Vybrat";
-            this.button1.UseVisualStyleBackColor = true;
+            button1.Location = new Point(153, 6);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Vybrat";
+            button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(705, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Konec";
-            this.button2.UseVisualStyleBackColor = true;
+            button2.Location = new Point(705, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Konec";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(5, 36);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(793, 402);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            pictureBox1.Location = new Point(5, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(793, 402);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1,
-            this.xStripStatusLabel,
-            this.toolStripStatusLabel2,
-            this.yStripStatusLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 447);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
-            this.statusStrip1.TabIndex = 4;
-            this.statusStrip1.Text = "statusStrip1";
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, xStripStatusLabel, toolStripStatusLabel2, yStripStatusLabel });
+            statusStrip1.Location = new Point(0, 447);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 4;
+            statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(17, 17);
-            this.toolStripStatusLabel1.Text = "X:";
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(17, 17);
+            toolStripStatusLabel1.Text = "X:";
             // 
             // xStripStatusLabel
             // 
-            this.xStripStatusLabel.Name = "xStripStatusLabel";
-            this.xStripStatusLabel.Size = new System.Drawing.Size(13, 17);
-            this.xStripStatusLabel.Text = "0";
+            xStripStatusLabel.Name = "xStripStatusLabel";
+            xStripStatusLabel.Size = new Size(13, 17);
+            xStripStatusLabel.Text = "0";
             // 
             // toolStripStatusLabel2
             // 
-            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(17, 17);
-            this.toolStripStatusLabel2.Text = "Y:";
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(17, 17);
+            toolStripStatusLabel2.Text = "Y:";
             // 
             // yStripStatusLabel
             // 
-            this.yStripStatusLabel.Name = "yStripStatusLabel";
-            this.yStripStatusLabel.Size = new System.Drawing.Size(13, 17);
-            this.yStripStatusLabel.Text = "0";
+            yStripStatusLabel.Name = "yStripStatusLabel";
+            yStripStatusLabel.Size = new Size(13, 17);
+            yStripStatusLabel.Text = "0";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(234, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "DX:";
+            label1.AutoSize = true;
+            label1.Location = new Point(234, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 15);
+            label1.TabIndex = 5;
+            label1.Text = "DX:";
             // 
             // dxTextBox
             // 
-            this.dxTextBox.Location = new System.Drawing.Point(265, 7);
-            this.dxTextBox.Name = "dxTextBox";
-            this.dxTextBox.Size = new System.Drawing.Size(100, 23);
-            this.dxTextBox.TabIndex = 6;
+            dxTextBox.Location = new Point(265, 7);
+            dxTextBox.Name = "dxTextBox";
+            dxTextBox.Size = new Size(100, 23);
+            dxTextBox.TabIndex = 6;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(380, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 15);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "DY:";
+            label2.AutoSize = true;
+            label2.Location = new Point(380, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 15);
+            label2.TabIndex = 7;
+            label2.Text = "DY:";
             // 
             // dyTextBox
             // 
-            this.dyTextBox.Location = new System.Drawing.Point(415, 5);
-            this.dyTextBox.Name = "dyTextBox";
-            this.dyTextBox.Size = new System.Drawing.Size(100, 23);
-            this.dyTextBox.TabIndex = 8;
+            dyTextBox.Location = new Point(415, 5);
+            dyTextBox.Name = "dyTextBox";
+            dyTextBox.Size = new Size(100, 23);
+            dyTextBox.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 469);
-            this.Controls.Add(this.dyTextBox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.dxTextBox);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.shapeComboBox);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 469);
+            Controls.Add(dyTextBox);
+            Controls.Add(label2);
+            Controls.Add(dxTextBox);
+            Controls.Add(label1);
+            Controls.Add(statusStrip1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(shapeComboBox);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- 2022/3.tyden/SimpleGraphics/SimpleGraphics/Form1.cs
+++ 2022/3.tyden/SimpleGraphics/SimpleGraphics/Form1.cs
@@ -1,36 +1,36 @@
-namespace SimpleGraphics
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace SimpleGraphics {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
+        private void pictureBox1_Click(object sender, EventArgs e) {
 
         }
 
-        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
             var dx = int.Parse(dxTextBox.Text);
             var dy = int.Parse(dyTextBox.Text);
             var x = e.Location.X;
             var y = e.Location.Y;
-            using (Graphics g = Graphics.FromHwnd(pictureBox1.Handle))
-            {
-                switch (shapeComboBox.SelectedIndex)
-                {
-                    case 0: g.DrawLine(Pens.Black, x, y, x + dx, y + dy);
+            using (Graphics g = Graphics.FromHwnd(pictureBox1.Handle)) {
+                switch (shapeComboBox.SelectedIndex) {
+                    case 0:
+                        g.DrawLine(Pens.Black, x, y, x + dx, y + dy);
                         break;
-                    case 1: g.DrawRectangle(Pens.Black, x,y, dx, dy);
+                    case 1:
+                        g.DrawRectangle(Pens.Black, x, y, dx, dy);
                         break;
-                    case 2: g.DrawEllipse(Pens.Black, x, y, dx, dy);
+                    case 2:
+                        g.DrawEllipse(Pens.Black, x, y, dx, dy);
                         break;
 
                 }
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Application.Exit();
         }
     }
 }
